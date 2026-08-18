7135496bb08b2120293ce625c447fb23119a1986
purrfectdoodle <purrfectdoodle@gmail.com>
2026-08-18 22:07:52 +0200

    WIP scripting Tamatown

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -52,12 +52,12 @@
 #else
 
 #define FLAG_HIDE_TRUBBISH   0x20 // Unused Flag
-#define FLAG_UNUSED_0x021    0x21 // Unused Flag
-#define FLAG_UNUSED_0x022    0x22 // Unused Flag
-#define FLAG_UNUSED_0x023    0x23 // Unused Flag
-#define FLAG_UNUSED_0x024    0x24 // Unused Flag
-#define FLAG_UNUSED_0x025    0x25 // Unused Flag
-#define FLAG_UNUSED_0x026    0x26 // Unused Flag
+#define FLAG_UNLOCKED_MEAL_HOT_DOG    0x21 // Unused Flag
+#define FLAG_UNLOCKED_MEAL_2    0x22 // Unused Flag
+#define FLAG_UNLOCKED_MEAL_3    0x23 // Unused Flag
+#define FLAG_UNLOCKED_SNACK_DOUGHNUT    0x24 // Unused Flag
+#define FLAG_UNLOCKED_SNACK_ICE_CREAM    0x25 // Unused Flag
+#define FLAG_UNLOCKED_SNACK_3    0x26 // Unused Flag
 #define FLAG_UNUSED_0x027    0x27 // Unused Flag
 #define FLAG_UNUSED_0x028    0x28 // Unused Flag
 #define FLAG_UNUSED_0x029    0x29 // Unused Flag
